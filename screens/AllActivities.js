61275cf2a5688d5bc688122011d6e3453522f088
Hanna Zhou <zhou.hanyi@northeastern.edu>
2024-02-05 17:19:47 -0800

    add ActivitiesList

--- screens/AllActivities.js
+++ screens/AllActivities.js
@@ -1,7 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { ActivitiesList } from "../components/ActivitiesList";
 
 export default function AllActivities() {
+  const activities = React.useContext(ActivitiesList);
+  console.log(activities);
   return (
     <View style={styles.container}>
       <Text>AllActivities</Text>
